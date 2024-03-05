1e1cbc3e8a004fa2be9882157a4a7865834f7f10
aforbush1 <aforbush17@gmail.com>
2024-03-04 19:14:55 -0700

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import collegeBasketballTeams from './CollegeBasketballTeams.json';
+
+interface CollegeProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1>Welcome to March Madness</h1>;
+}
+
+class CollegeBasketball extends React.Component<CollegeProps> {
+  render() {
+    const { school, name, city, state } = this.props;
+
+    return (
+      <div>
+        <img />
+        <h2>{school}</h2>
+        <h3>Mascot: {name}</h3>
+        <h3>Location: {city}, {state}</h3>
+      </div>
+    );
+  }
+}
+
+function CollegeTeamList() {
+  return (
+    <div>
+      {collegeBasketballTeams.map((oneTeam, index) => (
+        <CollegeBasketball key={index} {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <CollegeTeamList />
     </div>
   );
 }
